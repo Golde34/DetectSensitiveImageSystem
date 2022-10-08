50e0d0805c39fe0109622daadd96b72c23abf827
Golde34 <nguyendongducviet2001@gmail.com>
2022-10-08 16:10:54 +0700

    update backend
    update backend

--- backend_server/backend/backend/settings.py
+++ backend_server/backend/backend/settings.py
@@ -27,10 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
